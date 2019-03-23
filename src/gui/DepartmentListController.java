0d895e36b27071294d11a3352e5ac6086fd50028
rizidorio <rizidorio@outlook.com>
2019-03-23 09:51:15 -0300

    Controller e Service departamento

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,68 @@
+package gui;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+import model.services.DepartmentService;
+
+public class DepartmentListController implements Initializable {
+
+	private DepartmentService service;
+	
+	@FXML
+	private TableView<Department> tableViewDepartment;
+	
+	@FXML
+	private TableColumn<Department, Integer> tableColumnId;
+	
+	@FXML
+	private TableColumn<Department, String> tableColumnNome;
+	
+	@FXML
+	private Button btNovo;
+	
+	private ObservableList<Department> obsList;
+	
+	@FXML
+	public void onBtNovoAction() {
+		System.out.println("Cliacado");
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializaNode();
+	}
+
+	private void initializaNode() {
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnNome.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+	
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Serviço está nulo!");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
+	}
+}
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,54 +15,74 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemVendedor;
-	
+
 	@FXML
 	private MenuItem menuItemDepartamento;
-	
+
 	@FXML
 	private MenuItem menuItemSobre;
-	
+
 	@FXML
 	public void onMenuItemVendendorAction() {
 		System.out.println("onMenuItemVendendorAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartamentoAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
-	
+
 	@FXML
 	public void onMenuItemSobreAction() {
 		loadView("/gui/About.fxml");
 	}
-	
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
-		
+
 	}
-	
+
 	private synchronized void loadView(String name) {
 		try {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(name));
-		VBox newVbox = loader.load();
-		
-		Scene mainScene = Main.getMainScene();
-		VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-		
-		Node mainMenu = mainVBox.getChildren().get(0);
-		mainVBox.getChildren().clear();
-		mainVBox.getChildren().add(mainMenu);
-		mainVBox.getChildren().addAll(newVbox.getChildren());
-		}
-		catch(IOException e) {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(name));
+			VBox newVbox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVbox.getChildren());
+		} catch (IOException e) {
 			Alerts.showAlert("ERRO", null, e.getMessage(), AlertType.ERROR);
 		}
 	}
 
+	private synchronized void loadView2(String name) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(name));
+			VBox newVbox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVbox.getChildren());
+
+			DepartmentListController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		} catch (IOException e) {
+			Alerts.showAlert("ERRO", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
 }
